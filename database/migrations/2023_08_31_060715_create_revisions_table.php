4a377e1848f9c7e809369123c89cf7efee374aa7
Nic Barr <code@nicholasbarr.net>
2023-08-31 01:50:10 -0500

    wip checkpoint

---
+++ database/migrations/2023_08_31_060715_create_revisions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revisions', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('entry_id')->references('id')->on('entries');
+            $table->text('body')->nullable();
+            $table->text('body_html')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revisions');
+    }
+};
